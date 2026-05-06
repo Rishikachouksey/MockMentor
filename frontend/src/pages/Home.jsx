d85bee48fc3516b3fe003ad519b1cf2d286310ab
anjalideshmukh969 <anjalideshmukh2541@gmail.com>
2026-05-06 16:21:51 +0530

    added features

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -101,12 +101,6 @@ const Home = () => {
               desc: "Dynamic follow-up questions based on your answers."
             },
             {
-              icon: <BsRobot size={24} />,
-              step: "STEP-1",
-              title: "Role & Experience Selection",
-              desc: "AI adjusts difficulty based on selected job role"
-            },
-            {
               icon: <BsClock size={24} />,
               step: "STEP-3",
               title: "Time based simulation",
@@ -119,16 +113,39 @@ const Home = () => {
                 transition={{ duration: 0.6 + index * 0.2 }}
                 whileHover={{ rotate: 0, scale: 1.06 }}
 
-                className={`relative bg-white rounded-3xl border-2 border-green-100 
+                className={`relative bg-white rounded-3xl border-3 border-green-100 
               havor:border-green-500 p-10 w-80 max-w-[90%] shadow-md hover:shadow-2xl transition-all duration-300
               ${index === 0 ? "rotate-[-4deg]" : ""}
               ${index === 1 ? "rotate-[-3deg]" : ""}
               ${index === 0 ? "rotate-[-3deg]" : ""}
               `}>
+
+                <div className='absolute -top-8 left-1/2 -translate-x-1/2 bg-white border-2
+                 border-green-500 text-green-600 w-16 h-16 rounded-2xl flex items-center justify-center shadow-lg'>
+                  {item.icon}
+                </div>
+
+                <div className='pt-10 text-center'>
+                  <div className='text-xs text-green-600 font-semibold mb-2 tracking-wider'>{item.step}</div>
+                  <h3 className='font-semibold mb-3 text-lg'>{item.title}</h3>
+                  <p className='text-sm text-gray-500 leading-relaxed'>{item.desc}</p>
+                </div>
               </motion.div>
             ))
           }
         </div>
+
+        <div className='mb-32'>
+          <motion.h2 initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            className=' text-4xl font-semibold text-center mb-16'>
+              Advanced AI{" "}
+              <span className='text-green-600'>Capabilities</span>
+          </motion.h2>
+        </div>
+
+
       </div>
       {showAuth && <AuthModel onClose={() => setShowAuth(false)} />}
     </div>
